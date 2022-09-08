2117e3be8775623c23cc47ab306c9a5e93436acf
Fabio DrizZt <ing.fabioarg@gmail.com>
2022-09-08 15:58:07 -0300

    MVC finalizado

--- public/index.php
+++ public/index.php
@@ -1,18 +1,19 @@
 <?php
 
+use app\controllers\ProductController;
+use app\Router;
+
 require_once '../vendor/autoload.php';
 
-use app\Router;
-use app\controllers\ProductController;
-
-$router = new Router();
+$database = new \app\Database();
+$router = new Router($database);
 
 $router->get('/', [ProductController::class, 'index']);
 $router->get('/productos', [ProductController::class, 'index']);
 $router->get('/productos/crear', [ProductController::class, 'crear']);
 $router->post('/productos/crear', [ProductController::class, 'crear']);
-$router->get('/productos/editar', [ProductController::class, 'editar']);
-$router->post('/productos/editar', [ProductController::class, 'editar']);
+$router->get('/productos/actualizar', [ProductController::class, 'actualizar']);
+$router->post('/productos/actualizar', [ProductController::class, 'actualizar']);
 $router->post('/productos/borrar', [ProductController::class, 'borrar']);
 
 $router->resolve();
